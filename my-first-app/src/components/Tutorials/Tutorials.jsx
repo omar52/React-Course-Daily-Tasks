667c41255ad287863db212d06e724df96da3eab4
omar52 <omarrabdoo00@gmail.com>
2025-09-19 19:03:56 +0300

    uploading last changes

---
+++ my-first-app/src/components/Tutorials/Tutorials.jsx
@@ -0,0 +1,178 @@
+import "./Tutorial.css";
+const Tutorial = () => {
+  return (
+    <div className="tutorial-section container">
+      <div className="wrapper">
+        <div className="text">
+          <h2>resources and tutorıals</h2>
+          <p>
+            Welcome to the Resources and Tutorials section of DesignForge—a
+            treasure trove of knowledge and inspiration for digital artists and
+            enthusiasts. Whether you're looking to sharpen your skills, learn
+            new techniques, or dive deeper into the world of digital art, you'll
+            find everything you need right here. Explore our collection of
+            resources, tutorials, and guides curated to help you unlock your
+            creative potential and take your artistry to the next level.
+          </p>
+        </div>
+        <div className="row gx-5 gy-5">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-1.jpg" alt="" />
+              <div className="card-text">
+                <h2>Digital Painting 101</h2>
+                <p>
+                  Dive into the fundamentals of digital painting with our
+                  comprehensive guide. Learn essential techniques, brush
+                  settings, and color theory tips to create stunning digital
+                  artworks with confidence and precision.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-2.jpg" alt="" />
+              <div className="card-text">
+                <h2>Comprehensive</h2>
+                <p>
+                  Discover the secrets of effective composition and visual
+                  storytelling in our comprehensive tutorial. Learn to arrange
+                  elements in your artwork for captivating compositions that
+                  convey your message with clarity and impact.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-3.jpg" alt="" />
+              <div className="card-text">
+                <h2>Introduction to 3D Modeling</h2>
+                <p>
+                  Dive into the fundamentals of digital painting with our
+                  comprehensive guide. Learn essential techniques, brush
+                  settings, and color theory tips to create stunning digital
+                  artworks with confidence and precision.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-4.jpg" alt="" />
+              <div className="card-text">
+                <h2>Exploring Animation</h2>
+                <p>
+                  Elevate your art with our animation tutorial series, covering
+                  everything from keyframe basics to advanced rigging and motion
+                  graphics. Our step-by-step guides will empower you to master
+                  animation and infuse vitality into your creations.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="wrapper">
+        <div className="text">
+          <h2>HELPFUL GUIDES</h2>
+        </div>
+        <div className="row gx-5 gy-5">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-5.jpg" alt="" />
+              <div className="card-text">
+                <h2>Choosing the Right Digital Art Software:</h2>
+                <p>
+                  Dive into the fundamentals of digital painting with our
+                  comprehensive guide. Learn essential techniques, brush
+                  settings, and color theory tips to create stunning digital
+                  artworks with confidence and precision.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-6.jpg" alt="" />
+              <div className="card-text">
+                <h2>Navigating the Digital Art Market</h2>
+                <p>
+                  Ready to advance your art career? Our guide navigates the
+                  digital art market, offering insights and tips for promoting
+                  your work, enhancing your online presence, and monetizing your
+                  talents.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-7.jpg" alt="" />
+              <div className="card-text">
+                <h2>Understanding Color Theory</h2>
+                <p>
+                  Discover color theory's power to elevate your digital
+                  artworks. Master harmony, contrast, and symbolism for visually
+                  striking compositions that resonate with your audience.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="text">
+          <h2>INSPIRING WORKSHOPS</h2>
+        </div>
+        <div className="row gx-5 gy-5">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-8.jpg" alt="" />
+              <div className="card-text">
+                <h2>Choosing the Right Digital Art Software:</h2>
+                <p>
+                  Elevate your digital painting skills with our workshop series.
+                  Covering advanced rendering techniques and specialized
+                  subjects, our masterclasses unlock your full creative
+                  potential.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-9.jpg" alt="" />
+              <div className="card-text">
+                <h2>Character Design Bootcamp</h2>
+                <p>
+                  Join our intensive character design workshop for a unique
+                  journey. Our expert instructors will guide artists of all
+                  levels in crafting compelling characters brimming with depth,
+                  personality, and visual allure.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card">
+              <img className="w-100" src="card-10.jpg" alt="" />
+              <div className="card-text">
+                <h2>Artistic Entrepreneurship Seminar</h2>
+                <p>
+                  Discover color theory's power to elevate your digital
+                  artworks. Master harmony, contrast, and symbolism for visually
+                  striking compositions that resonate with your audience.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tutorial;
